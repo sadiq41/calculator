9cf100d8fbbd028d98e79c8d64d22c2a47e86f2c
sadiq41 <sadiqsonu41@gmail.com>
2022-07-31 12:18:56 +0530

    added functions for calculator

---
+++ script.js
@@ -0,0 +1,15 @@
+function cls()
+{
+    document.getElementById("result").value = "";
+}
+
+function display(value)
+{
+    document.getElementById("result").value += value;
+}
+function calculate()
+{
+    var a = document.getElementById("result").value;
+    var b = eval(a);
+    document.getElementById("result").value = b;
+}
